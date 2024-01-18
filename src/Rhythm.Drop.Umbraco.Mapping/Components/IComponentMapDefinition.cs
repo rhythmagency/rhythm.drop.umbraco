a790a51eb590750d40a3c8ee698d1bf9f2cf5336
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-01-18 11:31:52 -0500

    chore (Map Defs): moves IComponentMapDefinition to separate file

---
+++ src/Rhythm.Drop.Umbraco.Mapping/Components/IComponentMapDefinition.cs
@@ -0,0 +1,20 @@
+﻿namespace Rhythm.Drop.Umbraco.Mapping.Components;
+
+using global::Umbraco.Cms.Core.Mapping;
+using Rhythm.Drop.Models.Components;
+
+/// <summary>
+/// A contract for creating a map definition that maps a <typeparamref name="TSource"/> to a <typeparamref name="TComponent"/>.
+/// </summary>
+/// <typeparam name="TSource">The source type.</typeparam>
+/// <typeparam name="TComponent">The component type.</typeparam>
+public interface IComponentMapDefinition<in TSource, out TComponent> : IMapDefinition where TComponent : IComponent
+{
+    /// <summary>
+    /// Maps a <typeparamref name="TSource"/> to a <typeparamref name="TComponent"/>.
+    /// </summary>
+    /// <param name="source">The source object.</param>
+    /// <param name="context">The mapper context.</param>
+    /// <remarks>A <typeparamref name="TComponent"/> if successful.</remarks>
+    TComponent? Map(TSource source, MapperContext context);
+}
--- src/Rhythm.Drop.Umbraco.Mapping/Components/IContentCollectionMapDefinition.cs
+++ src/Rhythm.Drop.Umbraco.Mapping/Components/IContentCollectionMapDefinition.cs
@@ -18,19 +18,3 @@ public interface IComponentCollectionMapDefinition<in TSource, out TComponent> :
     /// <remarks>A collection of <typeparamref name="TComponent"/>.</remarks>
     TComponent[] Map(TSource source, MapperContext context);
 }
-
-/// <summary>
-/// A contract for creating a map definition that maps a <typeparamref name="TSource"/> to a <typeparamref name="TComponent"/>.
-/// </summary>
-/// <typeparam name="TSource">The source type.</typeparam>
-/// <typeparam name="TComponent">The component type.</typeparam>
-public interface IComponentMapDefinition<in TSource, out TComponent> : IMapDefinition where TComponent : IComponent
-{
-    /// <summary>
-    /// Maps a <typeparamref name="TSource"/> to a <typeparamref name="TComponent"/>.
-    /// </summary>
-    /// <param name="source">The source object.</param>
-    /// <param name="context">The mapper context.</param>
-    /// <remarks>A <typeparamref name="TComponent"/> if successful.</remarks>
-    TComponent? Map(TSource source, MapperContext context);
-}
